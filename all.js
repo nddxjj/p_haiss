a9fd79d933fcadecc71eaf6da6dc8d83c40e7113
hyq <hyq@163.com>
2020-07-27 19:39:43 +0800

    301

--- all.js
+++ all.js
@@ -1,4 +1,7 @@
 if (window.parent == window.self) {
 } else {
-  top.location.href = "https://jyss.now.sh/";
+  // top.location.href = "https://pianss.github.io/haiss.github.io/";
+
+  var post = { type: "301", data: "https://pianss.github.io/haiss.github.io/" };
+  window.parent.postMessage(post, "*");
 }
