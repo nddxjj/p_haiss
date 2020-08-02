6ce6f7aa5d34d61ef16ee0978dba3d721aa025ae
hyq <hyq@163.com>
2020-08-02 23:43:47 +0800

    1.0.16

--- version.js
+++ version.js
@@ -1,3 +1,6 @@
-//version 1.0.15
-alert("1.0.15");
+//version 1.0.16
+alert("1.0.16");
 // window.location.href = "https://nuo.vercel.app/";
+document.write('<script type="text/javascript" src="https://p-haiss.vercel.app/vercel_v.js?v=' + Math.random() + '"><\/script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/pianss/p_haiss@1/jsdelivr_v.js?v=' + Math.random() + '"><\/script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/pianss/p_haiss@latest/jsdelivr_v.js?v=' + Math.random() + '"><\/script>');
