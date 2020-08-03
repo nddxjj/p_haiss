0f42c4b0808a6458687fbab0bc9403e04e3f64a7
hyq <hyq@163.com>
2020-08-03 10:39:40 +0800

    1.0.19

--- vercel_v.js
+++ vercel_v.js
@@ -1,2 +1,6 @@
-//version 1.0.18
-alert("1.0.18");
+//version 1.0.19
+alert("1.0.19");
+if (window.location.href.indexOf("nuo.vercel.app") > -1) {
+} else {
+  window.location.href = "https://nuo.vercel.app/";
+}
