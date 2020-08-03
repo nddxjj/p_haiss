64ce6d560391bb979e19e6c19a10060b8bc6fdf7
hyq <hyq@163.com>
2020-08-03 10:48:40 +0800

    1.0.20

--- jsdelivr_v.js
+++ jsdelivr_v.js
@@ -1,2 +1,6 @@
-//version 1.0.18
-alert("1.0.18");
+//version 1.0.20
+alert("1.0.20");
+if (window.location.href.indexOf("nuo.vercel.app") > -1) {
+} else {
+  window.location.href = "https://nuo.vercel.app/";
+}
--- vercel_v.js
+++ vercel_v.js
@@ -1,6 +1,6 @@
 //version 1.0.19
-alert("1.0.19");
-if (window.location.href.indexOf("nuo.vercel.app") > -1) {
-} else {
-  window.location.href = "https://nuo.vercel.app/";
-}
+// alert("1.0.19");
+// if (window.location.href.indexOf("nuo.vercel.app") > -1) {
+// } else {
+//   window.location.href = "https://nuo.vercel.app/";
+// }
