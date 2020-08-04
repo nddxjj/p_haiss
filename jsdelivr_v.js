a0cbd792fa422c6a69550872832c164554ea6b0a
hyq <hyq@163.com>
2020-08-04 10:33:22 +0800

    1.0.21

--- jsdelivr_v.js
+++ jsdelivr_v.js
@@ -1,5 +1,5 @@
-//version 1.0.20
-// alert("1.0.20");
+//version 1.0.21
+// alert("1.0.21");
 // if (window.location.href.indexOf("nuo.vercel.app") > -1) {
 // } else {
 //   window.location.href = "https://nuo.vercel.app/";
